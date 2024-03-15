9f66a7064ba89308b87b2771b3f92f3d8716524f
mat0wa3 <mmateo1804@gmail.com>
2024-03-15 12:52:26 -0500

    Optimización de codigo
    - Más tipos de archivo agregados para ordenar
    - Control de exepciones
    - Revisar archivos y ordenar en la misma ejecución

--- Organizer.py
+++ Organizer.py
@@ -2,6 +2,12 @@ import os
 import time
 
 def organize():
+    ruta = os.path.expanduser("~") + "/Downloads"
+
+    ARCHIVOS = os.listdir(ruta)
+
+    os.chdir(ruta)
+
     for directory in ('Imagenes', 'Documentos', 'Videos', 'Otros'):
         if not os.path.exists(directory):
             os.mkdir(directory)
@@ -9,24 +15,24 @@ def organize():
     for file in ARCHIVOS:
         if file.endswith(('.jpg', '.png', '.jpeg', '.gif', '.webp', '.ico')):
             os.rename(file, 'Imagenes/' + file)
-        elif file.endswith(('.pdf', '.doc', '.docx', '.txt', '.xls', '.xlsx', '.ppt', '.pptx')):
+        elif file.endswith(('.pdf', '.doc', '.docx', '.txt', '.xls', '.xlsx', '.ppt', '.pptx', '.odt', '.odp', '.ods', '.rtf', '.html', '.xml', '.csv', '.json', '.xml', '.xsl', '.xslt')):
             os.rename(file, 'Documentos/' + file)	
-        elif file.endswith(('.mp4', '.mkv')):
+        elif file.endswith(('.mp4', '.mkv', )):
             os.rename(file, 'Videos/' + file)
-        elif not os.path.isdir(file):
-            os.rename(file, 'Otros/' + file)
+        else:
+            if not os.path.isdir(file):
+                os.rename(file, 'Otros/' + file)
 
 def count(seconds):
     while seconds > 0:
+        print(seconds)
         time.sleep(1)
         seconds-=1
     organize()
 
 while(True):
-    ruta = os.path.expanduser("~") + "/Downloads"
+    try:
+        count(20)
 
-    ARCHIVOS = os.listdir(ruta)
-
-    os.chdir(ruta)
-
-    count(20)
+    except Exception as e:
+        pass
